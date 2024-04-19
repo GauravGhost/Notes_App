351f43a4c4ee201e1c08f206b9916d9a3687eaa4
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-04-19 16:08:56 +0530

    Added Basic theme setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:notes_offline/models/note_database.dart';
 import 'package:notes_offline/pages/notes_page.dart';
+import 'package:notes_offline/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   // Initialize isar notes database
   WidgetsFlutterBinding.ensureInitialized();
   await NoteDatabase.initialize();
-  runApp(ChangeNotifierProvider(
-    create: (context) => NoteDatabase(),
+  runApp(MultiProvider(
+    providers: [
+      // Notes Provider
+      ChangeNotifierProvider(create: (context) => NoteDatabase()),
+      // Theme Provider
+      ChangeNotifierProvider(create: (context) => ThemeProvider()),
+    ],
     child: const MyApp(),
   ));
 }
@@ -19,6 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: Provider.of<ThemeProvider>(context).themeData,
       debugShowCheckedModeBanner: false,
       home: NotesPage(),
     );
---
+++ lib/theme/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade300,
+    primary: Colors.grey.shade200,
+    secondary: Colors.grey.shade400,
+    inversePrimary: Colors.grey.shade800,
+  ),
+);
+
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    background: Colors.grey.shade900,
+    primary: Colors.grey.shade800,
+    secondary: Colors.grey.shade700,
+    inversePrimary: Colors.grey.shade300,
+  ),
+);
